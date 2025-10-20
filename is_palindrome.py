2be10fd58d348cc861943d0d2366e426b92a5cd6
estolte <105747635+estolte@users.noreply.github.com>
2025-10-20 14:34:15 -0500

    Update is_palindrome.py

--- is_palindrome.py
+++ is_palindrome.py
@@ -1,4 +1,6 @@
 def is_palindrome(s: str) -> bool:
     """Return True if string s is a palindrome, ignoring case and spaces."""
+    ''''''
     cleaned = ''.join(c.lower() for c in s if c.isalnum())
     return cleaned == cleaned[::-1]
+    
