04e0ede342218bb650f76e17fb144ef6a8736546
estolte <105747635+estolte@users.noreply.github.com>
2025-10-24 14:17:34 -0500

    Update and rename is_palindrome.py to model.py

--- is_palindrome.py
+++
@@ -1,6 +0,0 @@
-def is_palindrome(s: str) -> bool:
-    """Return True if string s is a palindrome, ignoring case and spaces."""
-    ''''''
-    cleaned = ''.join(c.lower() for c in s if c.isalnum())
-    return cleaned == cleaned[::-1]
-    
---
+++ model.py
@@ -0,0 +1,2 @@
+def model(s: str) -> bool:
+    
